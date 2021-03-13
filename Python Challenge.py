62dec62206c74307689fb32293bd3526650fb300
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-03-14 01:34:53 +0530

    Added easy method

--- Python Challenge.py
+++ Python Challenge.py
@@ -13,3 +13,26 @@ avg = sum(number_list)/len(number_list)
 print ('Users: {} and {}'.format(name_1,name_2))
 print('Total Age: ',age_1+age_2)
 print('Average Age: ',avg,2)
+
+
+# nameOne = input("Enter 1st Name\n")
+# password = input("Enter password\n")
+# age1 = input("Enter your age")
+# print("Name:", nameOne)
+# print("Password:", password)
+# print("Age:", age1)
+# print("_________________________")
+# nameTwo = input("Enter 2nd Name\n")
+# password2 = input("Enter password\n")
+# age2 = input("Enter your age")
+# print("Name:", nameTwo)
+# print("Password:", password2)
+# print("Age:", age2)
+# print("_________________________")
+# print("Users:" + nameOne + " and " + nameTwo)
+# totalAge = int(age1) + int(age2);
+# average = totalAge /2;
+# print("Total Age:" , totalAge)
+# print("Average", average)
+
+#easy method XD by @reboot13-git
